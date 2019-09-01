b2f8bbcfba0410814c1cc04c689ba0964b86d088
Federico Lochbaum <Fede@SCVs-MacBook-Pro.local>
2019-09-01 10:45:22 -0300

    fix copy error

--- class-2/graph.py
+++ class-2/graph.py
@@ -1,5 +1,6 @@
 
 import random
+import copy
 
 start_delimiter_point = 0
 end_delimiter_point = 2
@@ -27,8 +28,8 @@ def generate_initial_node():
   return [possible_numbers[0:3], possible_numbers[3:6], possible_numbers[6:9]]
 
 def compute_next_state(node, newX, newY):
-  newNode = node
-  x, y = current_target_position(newNode)
+  newNode = copy.deepcopy(node)
+  x, y = current_target_position(node)
   newNode[x][y] = newNode[newX][newY]
   newNode[newX][newY] = 0
   return newNode
@@ -49,7 +50,7 @@ def is_valid_position(t):
 
 def possible_movements(node):
   x, y = current_target_position(node)
-  next_movements = [(x + 1, y), 'UP', (x - 1, y, 'DOWN'), (x, y + 1, 'RIGHT'), (x, y - 1, 'LEFT')]
+  next_movements = [(x + 1, y, 'UP'), (x - 1, y, 'DOWN'), (x, y + 1, 'RIGHT'), (x, y - 1, 'LEFT')]
   return list(filter(is_valid_position, next_movements))
 
 class EightProblemGraph:
@@ -57,21 +58,20 @@ class EightProblemGraph:
     self.graph = {}
 
   def __getitem__(self, node):
-    if node not in self.graph:
-      possible_movements = possible_movements(node)
-      self.graph[node] = {}
-      for newX, newY, direction in possible_movements:
-        self.graph[node].update({ direction: compute_next_state(node, newX, newY) })
+    str_node = str(node)
+    if str_node not in self.graph:
+      self.graph[str_node] = {}
+      for newX, newY, direction in possible_movements(node):
+        self.graph[str_node].update({ direction: compute_next_state(node, newX, newY) })
 
-    return self.graph[node]
+    return self.graph[str_node]
 
 
-print('generate_initial_node: ', str(generate_initial_node()))
-print('\n')
-print('generate_initial_node2: ', str(generate_initial_node()))
+initial = generate_initial_node()
+print('random initial state: ', str(initial))
 print('\n')
 
 graph = EightProblemGraph()
 print('graph: ', str(graph.graph))
 print('\n')
-print('graph[generate_initial_node()]: ', str(graph[generate_initial_node()]))
+print('graph[initial]: ', str(graph[initial]))
